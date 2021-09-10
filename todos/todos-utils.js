39b3d3c99b1f5f3567fc8fc052356e7f37d8d9af
Michael Rider <mikepdxrider@gmail.com>
2021-09-10 12:57:46 -0700

    tinkerings

--- todos/todos-utils.js
+++ todos/todos-utils.js
@@ -6,10 +6,21 @@ export function resetAndRenderTodoElements(todoArr, elDom){
     todoArr.forEach(item => {
 
         const elLi = document.createElement('li');
+        const elP = document.createElement('p');
+        // create an img
         
-        elLi.textContent = item.description;
-        
+        elP.textContent = item.description;
+        // set image src
         elLi.setAttribute('value', item.id);
+        
+        //image event listener
+            // Access todos
+                // Access storage
+                // Find user
+            // Find the correct todo
+                // DESTROY OBJ (splice)
+            // Save todos
+            // set div display: none
 
         if (item.completed === false){
             elLi.addEventListener('click', () => {
@@ -17,15 +28,16 @@ export function resetAndRenderTodoElements(todoArr, elDom){
                 elLi.style.textDecoration = 'line-through';
                 elLi.style.borderColor = 'rgba(128, 128, 128, 0.30)';
                 elLi.style.backgroundColor = 'rgba(167, 165, 165, 0.20)';
-                elLi.style.color = 'rgba(167, 165, 165, 0.20)';
+                elP.style.color = 'rgba(167, 165, 165, 0.20)';
             });
         } else { 
             elLi.style.textDecoration = 'line-through';
             elLi.style.borderColor = 'rgba(128, 128, 128, 0.30)';
             elLi.style.backgroundColor = 'rgba(167, 165, 165, 0.20)';
-            elLi.style.color = 'rgba(167, 165, 165, 0.20)';
+            elP.style.color = 'rgba(167, 165, 165, 0.20)';
         }
         
+        elLi.append(elP);
         elDom.append(elLi); 
     });        
 }
